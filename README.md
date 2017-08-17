bd1ab6d544177ccce154ff2592c1bbded380eab1
Christian <christian.kreiling@gmail.com>
2017-08-17 16:35:12 -0500

    Models have been created. Still need to write tests but backbone of the application is in plcae.

--- README.md
+++ README.md
@@ -1,26 +1,16 @@
-# django-react-template
-Template for any Django-React based project with minimal setup required.
+# payment-collection-prototype
+This is a payment collections (mostly geared toward Venmo) server written using Django REST framework.
 
-This setup is based on the tutorial found at <a href=https://hackernoon.com/creating-websites-using-react-and-django-rest-framework-b14c066087c7>this link.</a>
-
-Make sure to install all of the project's dependencies:
+For development, clone the project and run:
 <br>
 <code>pip install -r requirements.txt</code>
 <br>
 <code>cd frontend</code>
 <br>
 <code>npm install</code>
+<br>
 
-In development, at least at the moment, the frontend runs from localhost:3000 and the backend runs from localhost:8000. To start the frontend, do the following:
+You should be ready to start building on top of this project after that.
 <br>
-<code>cd frontend</code>
-<br>
-<code>npm start</code>
-
-And to start the backend run:
-<br>
-<code>cd backend</code>
-<br>
-<code>python manage.py runserver</code>
-
-This uses Django-CORS-Headers module to allow cross-origin requests in development, however most will probably serve both from the same origin in production.
+Branching in this project has been done for the most part chronologically to allow me to go back to past versions and 
+implement features differently.
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,93 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """
+    Extends Django's User object to create a profile for the user. This profile has all of the client-facing material
+    attached to it:
+        - Payers
+        - Payment Plans
+    It also has all of the non-client-facing material attached to it, specifically for Venmo's Oauth2.0 setup.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    venmo_handle = models.CharField()
+    venmo_auth_token = models.CharField()
+    venmo_refresh_token = models.CharField()
+    # Random code for the survey that retrieves the organization's users
+    survey_code = models.CharField(unique=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class PaymentPlanOption(models.Model):
+    """
+    Defines a payment plan option, to be selected by a payer upon filling out the corresponding survey.
+    """
+    option_name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    class Meta:
+        order_with_respect_to = 'user'
+
+    def __str__(self):
+        return self.option_name
+
+
+class Payment(models.Model):
+    """
+    Defines a single payment, with the date that it is due and the amount that is due on that date. A foreign key
+    associates many of these with one PaymentPlanOption.
+    """
+    date_due = models.DateTimeField()
+    amount_due = models.DecimalField(decimal_places=2)
+    # The PaymentPlanOption this payment is associated with
+    payment_plan = models.ForeignKey(PaymentPlanOption, on_delete=models.CASCADE)
+
+    class Meta:
+        order_with_respect_to = 'payment_plan'
+
+    def __str__(self):
+        return self.date_due.__str__() + ', ' + self.amount_due.__str__()
+
+
+class Payer(models.Model):
+    """
+    These represent a single payer in an organization. These are associated with a UserProfile so the collector
+    can easily request funds from them based on their chosen payment plan.
+    """
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    payment_plan = models.ForeignKey(PaymentPlanOption)
+    venmo_username = models.CharField(blank=True, unique=True, verbose_name="payer's venmo username")
+    email = models.EmailField()
+    phone_number = models.CharField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    # Information regarding the last time the user had paid
+    last_pay_date = models.DateTimeField(null=True, verbose_name="last date the payer paid")
+    last_pay_amount = models.DecimalField(decimal_places=2, null=True, verbose_name="last transaction amount")
+    next_pay_date = models.DateTimeField(null=True)
+    next_pay_amount = models.DecimalField(decimal_places=2, null=True)
+    total_paid = models.DecimalField(decimal_places=2, verbose_name="total paid to the organization")
+    # The user this Payer is identified with
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name + ': ' + self.payment_plan.__str__()
+
+    class Meta:
+        order_with_respect_to = 'user'
+
+
+class Transaction(models.Model):
+    """
+    A transaction made by the Payer. One of these is automatically generated whenever a Payer makes a Venmo transaction.
+    """
+    date = models.DateTimeField(verbose_name="date of transaction")
+    amount = models.DecimalField(decimal_places=2, verbose_name="transaction amount")
+    payer = models.ForeignKey(Payer, on_delete=models.CASCADE)
+
+    class Meta:
+        order_with_respect_to = 'payer'
